e3e7658ffc6df0a0f5aab5fef52bdd6dfb5b043b
DiogoCezario-Dev <152320636+DiogoCezario-Dev@users.noreply.github.com>
2023-12-08 16:17:43 -0300

    Fim do projeto

--- yag/yag.js
+++ yag/yag.js
@@ -1,2 +1,22 @@
-var nAleatorio = Math.floor(Math.random() * 20)
-alert(nAleatorio)
+var botaoSIm = document.getElementById("button-nao")
+
+botaoSIm.addEventListener("mouseenter", clickSim)
+
+function clickSim(){
+    var localInput = document.getElementById("container")
+    var nAleatorio = Math.floor(Math.random() * 500)
+    localInput.innerHTML = `
+    <div id="content">
+    <h1>Você é gay?</h1>
+
+    <div id="container-input">
+            <button type="submit" id="button-sim">Sim</button>
+            <button onmouseenter="clickSim()"style="position: absolute; top: ${nAleatorio}px; left: ${nAleatorio}px;" type="submit" id="button-nao">Não</button>
+    </div>
+</div>
+    `
+}
+
+function verdade(){
+    alert("Isso é verdade, todos nos sabemos")
+}
