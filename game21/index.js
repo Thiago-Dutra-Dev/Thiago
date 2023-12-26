05dd19fc2778f0f10c998520eeabdb08127cd243
DiogoCezario-Dev <152320636+DiogoCezario-Dev@users.noreply.github.com>
2023-12-26 00:05:17 -0300

    main

--- game21/index.js
+++ game21/index.js
@@ -1,6 +1,8 @@
 //OBJETO JOGADOR 01
 var p1 = {
-    num: 0
+    num: 0,
+    vitorias: 0,
+    derrotas: 0
 }
 
 const btnAddNumP1 = document.getElementById("btn-add-nump1").addEventListener("click", addNumP1)   
@@ -21,7 +23,8 @@ function addNumP1(){
     if(p1.num > 21){
         const localNumP1 = document.getElementById("local-numP1")
         localNumP1.innerHTML = `<p>${p1.num}</p> <p>Você perdeu</p>`
-
+        p1.derrotas++
+        p2.vitorias++
     }
 }
 
@@ -29,7 +32,9 @@ function addNumP1(){
 
 //OBJETO JOGADOR 01
 var p2 = {
-    num: 0
+    num: 0,
+    vitorias: 0,
+    derrotas: 0
 }
 
 const btnAddNumP2 = document.getElementById("btn-add-nump2").addEventListener("click", addNumP2)   
@@ -50,6 +55,7 @@ function addNumP2(){
     if(p2.num > 21){
         const localNumP2 = document.getElementById("local-numP2")
         localNumP2.innerHTML = `<p>${p2.num}</p> <p>Você perdeu</p>`
-
+        p2.derrotas++
+        p1.vitorias++
     }
 }
